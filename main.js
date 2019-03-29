fe151ed6de1829ad3c3ff2857bb6602ea2c5d3f9
DEJIDOPE <dejidope@gmail.com>
2019-03-29 13:58:09 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// function rectangleArea(width, height) {
+//     let area = width * height;
+//     return area; 
+//   }
+//   console.log(rectangleArea(5, 7)); 
+
+// function rectangleArea(width, height) {
+//     if (width < 0 || height < 0) {
+//       return 'You need positive integers to calculate area!';
+//     }
+//     return width * height;
+//   }
+//   rectangleArea(5,7);
+
+// function monitorCount(rows, columns) {
+//     let number = rows * columns;
+//     return number;
+//   }
+//   monitorCount(5,4);
+
